ce8444debb674ac1eaa6c04d7260052280427b1a
Jonathan Foote <foote@fastly.com>
2021-05-17 13:06:25 -0400

    add property-based testing of cookie parser

--- src/cookies.rs
+++ src/cookies.rs
@@ -37,3 +37,41 @@ pub fn session(name: &str, value: &str) -> String {
         COOKIE_PREFIX, name, value, COOKIE_ATTRIBUTES
     )
 }
+
+/*
+#[cfg(test)]
+use quickcheck::quickcheck; // Rust 2018 style
+use cookie::Cookie;
+*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    quickcheck::quickcheck! {
+        fn cookie_parsers_agree(cookie_string: String) -> bool {
+            let fastly_cookie = parse(&cookie_string);
+            match cookie::Cookie::parse(cookie_string.clone()) {
+                Err(cookie::ParseError::EmptyName) => match fastly_cookie.get("") {
+                    Some(_) => true,
+                    None => false
+                },
+                Err(_) => fastly_cookie.is_empty(),
+                Ok(cookie) => fastly_cookie.get(cookie.name()).unwrap() == &cookie.value()
+            }
+        }
+    }
+
+    #[test]
+    fn test_fail() {
+        // Demo to show input when a property test fails
+        let cookie_string = "\u{0}=;";
+        let fastly_cookie = parse(&cookie_string);
+        println!("fastly_cookie={:?}", fastly_cookie);
+        let cookie_result = cookie::Cookie::parse(cookie_string.clone());
+        println!("cookie={:?}", cookie_result);
+        let cookie = cookie_result.unwrap();
+        println!("cooke.name={:?}, cookie.value={:?}", cookie.name(), cookie.value());
+        assert!(true);
+    }
+}
